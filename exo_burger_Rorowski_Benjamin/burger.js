bf3ce998696d35e89c5650ebb4a252c3a49fe333
bengrid <benjamin.rorowski@gmail.com>
2022-09-16 09:44:44 +0200

    Fix

--- exo_burger_Rorowski_Benjamin/burger.js
+++ exo_burger_Rorowski_Benjamin/burger.js
@@ -1,19 +1,26 @@
-const burger = ["pain","chedar","steak","cornichon","ketchup","salade","tomate","oignon"]
-burger.splice(2,1);
-console.log(`burger vegie: ${burger}`);
+const burgerO = ["pain","chedar","steak","cornichon","ketchup","salade","tomate","oignon"]
 
-burger.splice(1,1)
-console.log(`burger vegan: ${burger}`);
+let burgerVegie = [...burgerO];
+burgerVegie.splice(2,1);
+console.log(`burger vegie: ${burgerVegie}`);
 
-burger.splice(1,2,"chedar","steak");
-burger.splice(3,1,);
-burger.splice(3,1,);
-burger.splice(3,1,);
-console.log(`burger carnivore: ${burger}`);
+let burgerVegan = [...burgerO];
+burgerVegan.splice(1,1)
+burgerVegan.splice(1,1)
+console.log(`burger vegan: ${burgerVegan}`);
 
-burger.splice(3,2,"chedar","steak");
-burger.splice(5,3,"salade","oignon","ketchup");
-console.log(`burger custom: ${burger}`);
+let burgerCarnivore = [...burgerO];
+burgerCarnivore.splice(1,2,"chedar","steak");
+burgerCarnivore.splice(3,1,);
+burgerCarnivore.splice(4,1,);
+burgerCarnivore.splice(4,1,);
+burgerCarnivore.splice(4,1,);
+console.log(`burger carnivore: ${burgerCarnivore}`);
+
+let burgerCustom = [...burgerO];
+burgerCustom.splice(3,2,"chedar","steak");
+burgerCustom.splice(5,3,"salade","oignon","ketchup");
+console.log(`burger custom: ${burgerCustom}`);
 
 let commande = [];
 let x = prompt("combien d'elements voulez vous?: ");
@@ -24,7 +31,8 @@ for(let i=0;i!=nb;i++){
     commande.push(element);
 }
 if(commande.length>0){
-    console.log(commande);
+    alert(commande);
+    console.log(`burger custom: ${commande}`)
 }
 
 
